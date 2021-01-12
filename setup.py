69cd21066ba52d780b228d5ef5ddc5ca06355cb4
Felipe Lolas <felipe.lolas@bci.cl>
2021-01-11 22:54:23 -0300

    fix rst

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/flolas/apache_airflow_providers_teradata',
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
 )
